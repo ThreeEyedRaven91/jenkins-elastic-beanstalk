160c8e66784bb0857de3edb86c923da12897de8f
EugeneNguyen <xuanbinh91@gmail.com>
2018-11-16 16:46:39 +0700

    no message

--- vars/terEB.groovy
+++ vars/terEB.groovy
@@ -1,7 +1,7 @@
 def deploy(args) {
     args.script.sh 'mkdir -p .elasticbeanstalk'
     args.script.sh '> .elasticbeanstalk/config.yml'
-    def file = new File('.elasticbeanstalk/config.yml')
+    def file = new File('./.elasticbeanstalk/config.yml')
     file << """
 branch-defaults:
   develop:
